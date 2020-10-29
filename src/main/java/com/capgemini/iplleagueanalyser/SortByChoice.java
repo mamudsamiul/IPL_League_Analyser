47ac1497d96c56eb82abfebddb72175cb732d2fb
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-29 23:58:14 +0530

    (Samiul Mamud) Added: Ability to find best batsman with best avg and great SR

--- src/main/java/com/capgemini/iplleagueanalyser/SortByChoice.java
+++ src/main/java/com/capgemini/iplleagueanalyser/SortByChoice.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class SortByChoice<T> {
 	public enum Choice {
-		BAT_AVG, STRIKE_RATE, MAX_FOURS, MAX_SIXES, MAX_BOUNDARIES_AND_SR;
+		BAT_AVG, STRIKE_RATE, MAX_FOURS, MAX_SIXES, MAX_BOUNDARIES_AND_SR, MAX_AVG_AND_SR;
 	}
 
 	public Choice choice;
@@ -42,6 +42,12 @@ public class SortByChoice<T> {
 		case "MAX_BOUNDARIES_AND_SR": {
 			Comparator<IPLLeagueBatsman> compareBy = Comparator.comparing(IPLLeagueBatsman::StrikeRate)
 					.thenComparing(IPLLeagueBatsman::noOfBoundaries);
+			Collections.sort(list1, compareBy.reversed());
+			return (List<T>) list1;
+		}
+		case "MAX_AVG_AND_SR": {
+			Comparator<IPLLeagueBatsman> compareBy = Comparator.comparing(IPLLeagueBatsman::Average)
+					.thenComparing(IPLLeagueBatsman::StrikeRate);
 			Collections.sort(list1, compareBy.reversed());
 			return (List<T>) list1;
 		}
