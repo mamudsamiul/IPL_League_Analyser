89f3b8e2e225f539a102942104e3e82c0551a35c
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-29 22:49:33 +0530

    (Samiul Mamud) Created: Welcome to The IPL League Analyser

---
+++ src/main/java/com/capgemini/iplleagueanalyser/App.java
@@ -0,0 +1,7 @@
+package com.capgemini.iplleagueanalyser;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome To IPL League Analyser !!");
+	}
+}
---
+++ src/test/java/com/capgemini/iplleagueanalyser/AppTest.java
@@ -0,0 +1,8 @@
+package com.capgemini.iplleagueanalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+}
